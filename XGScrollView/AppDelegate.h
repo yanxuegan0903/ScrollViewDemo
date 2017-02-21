60bd71eb3d89e5c9f9158e0be103f5c1b0da850f
yanxuegan <yanxuegan0903@163.com>
2017-02-21 09:21:29 +0800

    Initial Commit

---
+++ XGScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XGScrollView
+//
+//  Created by vsKing on 2017/2/21.
+//  Copyright © 2017年 vsKing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
